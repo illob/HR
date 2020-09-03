86710c4e781c70376c7774cfae6edd3458d6c127
illob <56627550+illob@users.noreply.github.com>
2020-09-03 10:01:39 +0000

    Implementation of algorithm

--- max_int.py
+++ max_int.py
@@ -2,3 +2,14 @@
 # negative value is entered, breaking the loop.
 # after an int is entered it is compared to the
 # current max_int and replaces it if it is bigger
+
+max_int = 0
+while True:
+    num_int = int(input("Input a number: "))    # Do not change this line
+    # Fill in the missing code
+    if num_int < 0:
+        break
+    if num_int > max_int:
+        max_int = num_int
+
+print("The maximum is", max_int)    # Do not change this line
