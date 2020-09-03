f75114549f744fab6df314942d44db4815c33d01
illob <56627550+illob@users.noreply.github.com>
2020-09-03 09:58:28 +0000

    Algorithm written for max_int

---
+++ max_int.py
@@ -0,0 +1,4 @@
+# the program keeps asking for an int until a 
+# negative value is entered, breaking the loop.
+# after an int is entered it is compared to the
+# current max_int and replaces it if it is bigger
