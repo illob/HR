907891ee1b513a2996de2f3c4acec4981842d32e
illob <56627550+illob@users.noreply.github.com>
2020-09-03 10:05:57 +0000

    Algorithm written for sequence

---
+++ sequence.py
@@ -0,0 +1,7 @@
+#Design an algorithm that generates the first n numbers in the following sequence:; 1, 2, 3, 6, 11, 20, 37, ___, ___, ___, …
+
+#1. ask for three consecutive numbers (123)
+#2. add them together to get the fourth number (6)
+#3. take the fourth (newest) number and add it together with the 2 numbers ahead of it (2+3+6=11)
+#4. repeat step 3 n times
+
