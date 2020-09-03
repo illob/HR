852514b5217445f2e05b4d8b1fff27d925f610b9
illob <56627550+illob@users.noreply.github.com>
2020-09-03 10:16:35 +0000

    Algorithm implemented

--- sequence.py
+++ sequence.py
@@ -5,3 +5,19 @@
 #3. take the fourth (newest) number and add it together with the 2 numbers ahead of it (2+3+6=11)
 #4. repeat step 3 n times
 
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+first_int = 1
+second_int = 2
+third_int = 3
+sum = 0
+
+for n in range (1,n+1):
+	if n < 4:
+		print(n)
+	else:
+		sum = first_int + second_int + third_int
+		print(sum)
+		first_int = second_int
+		second_int = third_int
+		third_int = sum
